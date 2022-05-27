6ff1e4aa77100eb673fa082763a3b17fdd7a90d0
Tony Day <tonyday567@gmail.com>
2022-05-27 13:14:30 +1000

    ormolu inlines

--- app/inlines.hs
+++ app/inlines.hs
@@ -1,38 +1,42 @@
-{-# OPTIONS_GHC -Wall #-}
 {-# LANGUAGE BangPatterns #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# OPTIONS_GHC -Wall #-}
 
 -- | basic measurement and callibration
 module Main where
 
-import Prelude hiding ((.))
-import Perf
 import Control.Category
-import Control.Monad
-import Options.Applicative
 import Control.DeepSeq
-import Data.Text (Text)
-import qualified Data.Map.Strict as Map
+import Control.Monad
 import Control.Monad.State.Lazy
 import Data.FormatN
+import qualified Data.Map.Strict as Map
+import Data.Text (Text)
+import Options.Applicative
+import Perf
+import Prelude hiding ((.))
 
 data Options = Options
   { optionRuns :: Int,
     optionLength :: Int,
     optionAlgoType :: AlgoType,
     optionStatDType :: StatDType
-  } deriving (Eq, Show)
+  }
+  deriving (Eq, Show)
 
 options :: Parser Options
-options = Options <$>
-  option auto (value 1000 <> long "runs" <> short 'r' <> help "number of runs to perform") <*>
-  option auto (value 1000 <> long "length" <> short 'l' <> help "length of list") <*>
-  parseAlgo <*>
-  parseStatD
+options =
+  Options
+    <$> option auto (value 1000 <> long "runs" <> short 'r' <> help "number of runs to perform")
+    <*> option auto (value 1000 <> long "length" <> short 'l' <> help "length of list")
+    <*> parseAlgo
+    <*> parseStatD
 
 opts :: ParserInfo Options
-opts = info (options <**> helper)
-  (fullDesc <> progDesc "perf benchmarking" <> header "basic perf callibration")
+opts =
+  info
+    (options <**> helper)
+    (fullDesc <> progDesc "perf benchmarking" <> header "basic perf callibration")
 
 -- * in-module variations on a tick counter
 
@@ -123,8 +127,8 @@ ticksNoPragma n0 f a = go f a n0 []
     go f' a' n ts
       | n <= 0 = pure (ts, f a)
       | otherwise = do
-        (t, _) <- tickNoPragma f a
-        go f' a' (n - 1) (t:ts)
+          (t, _) <- tickNoPragma f a
+          go f' a' (n - 1) (t : ts)
 
 ticksLazy :: Int -> (a -> b) -> a -> IO ([Cycles], b)
 ticksLazy n0 f a = go f a n0 []
@@ -132,8 +136,8 @@ ticksLazy n0 f a = go f a n0 []
     go f' a' n ts
       | n <= 0 = pure (ts, f a)
       | otherwise = do
-        (t, _) <- tickLazy f a
-        go f' a' (n - 1) (t:ts)
+          (t, _) <- tickLazy f a
+          go f' a' (n - 1) (t : ts)
 
 ticksInline :: Int -> (a -> b) -> a -> IO ([Cycles], b)
 ticksInline n0 f a = go f a n0 []
@@ -141,8 +145,8 @@ ticksInline n0 f a = go f a n0 []
     go f' a' n ts
       | n <= 0 = pure (ts, f a)
       | otherwise = do
-        (t, _) <- tickInline f a
-        go f' a' (n - 1) (t:ts)
+          (t, _) <- tickInline f a
+          go f' a' (n - 1) (t : ts)
 
 ticksInlineable :: Int -> (a -> b) -> a -> IO ([Cycles], b)
 ticksInlineable n0 f a = go f a n0 []
@@ -150,8 +154,8 @@ ticksInlineable n0 f a = go f a n0 []
     go f' a' n ts
       | n <= 0 = pure (ts, f a)
       | otherwise = do
-        (t, _) <- tickInlineable f a
-        go f' a' (n - 1) (t:ts)
+          (t, _) <- tickInlineable f a
+          go f' a' (n - 1) (t : ts)
 
 ticksNoinline :: Int -> (a -> b) -> a -> IO ([Cycles], b)
 ticksNoinline n0 f a = go f a n0 []
@@ -159,8 +163,8 @@ ticksNoinline n0 f a = go f a n0 []
     go f' a' n ts
       | n <= 0 = pure (ts, f a)
       | otherwise = do
-        (t, _) <- tickNoinline f a
-        go f' a' (n - 1) (t:ts)
+          (t, _) <- tickNoinline f a
+          go f' a' (n - 1) (t : ts)
 
 ticksInline1 :: Int -> (a -> b) -> a -> IO ([Cycles], b)
 ticksInline1 n0 f a = go f a n0 []
@@ -168,8 +172,8 @@ ticksInline1 n0 f a = go f a n0 []
     go f' a' n ts
       | n <= 0 = pure (ts, f a)
       | otherwise = do
-        (t, _) <- tickInline1 f a
-        go f' a' (n - 1) (t:ts)
+          (t, _) <- tickInline1 f a
+          go f' a' (n - 1) (t : ts)
 
 ticksInline2 :: Int -> (a -> b) -> a -> IO ([Cycles], b)
 ticksInline2 n0 f a = go f a n0 []
@@ -177,8 +181,8 @@ ticksInline2 n0 f a = go f a n0 []
     go f' a' n ts
       | n <= 0 = pure (ts, f a)
       | otherwise = do
-        (t, _) <- tickInline2 f a
-        go f' a' (n - 1) (t:ts)
+          (t, _) <- tickInline2 f a
+          go f' a' (n - 1) (t : ts)
 
 ticksInline1' :: Int -> (a -> b) -> a -> IO ([Cycles], b)
 ticksInline1' n0 f a = go f a n0 []
@@ -186,8 +190,8 @@ ticksInline1' n0 f a = go f a n0 []
     go f' a' n ts
       | n <= 0 = pure (ts, f a)
       | otherwise = do
-        (t, _) <- tickInline1' f a
-        go f' a' (n - 1) (t:ts)
+          (t, _) <- tickInline1' f a
+          go f' a' (n - 1) (t : ts)
 
 ticksInline2' :: Int -> (a -> b) -> a -> IO ([Cycles], b)
 ticksInline2' n0 f a = go f a n0 []
@@ -195,41 +199,41 @@ ticksInline2' n0 f a = go f a n0 []
     go f' a' n ts
       | n <= 0 = pure (ts, f a)
       | otherwise = do
-        (t, _) <- tickInline2' f a
-        go f' a' (n - 1) (t:ts)
+          (t, _) <- tickInline2' f a
+          go f' a' (n - 1) (t : ts)
 
 -- | run the various incarnations of tick.
 runInlines :: (NFData t, NFData b) => Text -> (t -> b) -> t -> Int -> StatDType -> StateT (Map.Map [Text] Double) IO ()
 runInlines l f a n s = do
-  addStat [l,"tick"] . statD s =<< lift (fst <$> multi tick n f a)
-  addStat [l,"tickWHNF"] . statD s =<< lift (fst <$> multi tickWHNF n f a)
-  addStat [l,"tickLazy"] . statD s =<< lift (fst <$> multi tickLazy n f a)
-  addStat [l,"tickForce"] . statD s =<< lift (fst <$> multi tickForce n f a)
-  addStat [l,"tickForceArgs"] . statD s =<< lift (fst <$> multi tickForceArgs n f a)
-  addStat [l,"stepTime"] . statD s =<< lift (snd . head . Map.toList <$> execPerfT (toMeasureN n stepTime) (f |$| a))
-  addStat [l,"times"] . statD s =<< lift (snd . head . Map.toList <$> execPerfT (times n) (f |$| a))
-  addStat [l,"ticksNoPragma"] . statD s =<< lift (fst <$> ticksNoPragma n f a)
-  addStat [l,"ticksLazy"] . statD s =<< lift (fst <$> ticksLazy n f a)
-  addStat [l,"ticksInlineable"] . statD s =<< lift (fst <$> ticksInlineable n f a)
-  addStat [l,"ticksNoinline"] . statD s =<< lift (fst <$> ticksNoinline n f a)
-  addStat [l,"ticksInline1"] . statD s =<< lift (fst <$> ticksInline1 n f a)
-  addStat [l,"ticksInline2"] . statD s =<< lift (fst <$> ticksInline2 n f a)
-  addStat [l,"ticksInline1'"] . statD s =<< lift (fst <$> ticksInline1' n f a)
-  addStat [l,"ticksInline2'"] . statD s =<< lift (fst <$> ticksInline2' n f a)
-  addStat [l,"multi_tickNoPragma"] . statD s =<< lift (fst <$> multi tickNoPragma n f a)
-  addStat [l,"multi_tickInlineable"] . statD s =<< lift (fst <$> multi tickInlineable n f a)
-  addStat [l,"multi_tickNoinline"] . statD s =<< lift (fst <$> multi tickNoinline n f a)
-  addStat [l,"multi_tickInline1"] . statD s =<< lift (fst <$> multi tickInline1 n f a)
-  addStat [l,"multi_tickInline2"] . statD s =<< lift (fst <$> multi tickInline2 n f a)
-  addStat [l,"multi_tickInline1'"] . statD s =<< lift (fst <$> multi tickInline1' n f a)
-  addStat [l,"multi_tickInline2'"] . statD s =<< lift (fst <$> multi tickInline2' n f a)
+  addStat [l, "tick"] . statD s =<< lift (fst <$> multi tick n f a)
+  addStat [l, "tickWHNF"] . statD s =<< lift (fst <$> multi tickWHNF n f a)
+  addStat [l, "tickLazy"] . statD s =<< lift (fst <$> multi tickLazy n f a)
+  addStat [l, "tickForce"] . statD s =<< lift (fst <$> multi tickForce n f a)
+  addStat [l, "tickForceArgs"] . statD s =<< lift (fst <$> multi tickForceArgs n f a)
+  addStat [l, "stepTime"] . statD s =<< lift (snd . head . Map.toList <$> execPerfT (toMeasureN n stepTime) (f |$| a))
+  addStat [l, "times"] . statD s =<< lift (snd . head . Map.toList <$> execPerfT (times n) (f |$| a))
+  addStat [l, "ticksNoPragma"] . statD s =<< lift (fst <$> ticksNoPragma n f a)
+  addStat [l, "ticksLazy"] . statD s =<< lift (fst <$> ticksLazy n f a)
+  addStat [l, "ticksInlineable"] . statD s =<< lift (fst <$> ticksInlineable n f a)
+  addStat [l, "ticksNoinline"] . statD s =<< lift (fst <$> ticksNoinline n f a)
+  addStat [l, "ticksInline1"] . statD s =<< lift (fst <$> ticksInline1 n f a)
+  addStat [l, "ticksInline2"] . statD s =<< lift (fst <$> ticksInline2 n f a)
+  addStat [l, "ticksInline1'"] . statD s =<< lift (fst <$> ticksInline1' n f a)
+  addStat [l, "ticksInline2'"] . statD s =<< lift (fst <$> ticksInline2' n f a)
+  addStat [l, "multi_tickNoPragma"] . statD s =<< lift (fst <$> multi tickNoPragma n f a)
+  addStat [l, "multi_tickInlineable"] . statD s =<< lift (fst <$> multi tickInlineable n f a)
+  addStat [l, "multi_tickNoinline"] . statD s =<< lift (fst <$> multi tickNoinline n f a)
+  addStat [l, "multi_tickInline1"] . statD s =<< lift (fst <$> multi tickInline1 n f a)
+  addStat [l, "multi_tickInline2"] . statD s =<< lift (fst <$> multi tickInline2 n f a)
+  addStat [l, "multi_tickInline1'"] . statD s =<< lift (fst <$> multi tickInline1' n f a)
+  addStat [l, "multi_tickInline2'"] . statD s =<< lift (fst <$> multi tickInline2' n f a)
 
 main :: IO ()
 main = do
   o <- execParser opts
   let !n = optionRuns o
   let !l = optionLength o
-  let !ls = [1..l]
+  let !ls = [1 .. l]
   let s = optionStatDType o
   let !a = optionAlgoType o
   _ <- warmup 100
